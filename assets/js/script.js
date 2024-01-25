423cbf6d29c749d61730bb1c9d807a79b58ca839
mmetrodw <artgure@gmail.com>
2024-01-25 22:02:36 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,141 @@
+// BezierEasing https://github.com/gre/bezier-easing
+
+const BezierEasing = (() => {
+
+  const kSplineTableSize = 11;
+  const kSampleStepSize = 1.0 / (kSplineTableSize - 1.0);
+
+  function A(aA1, aA2) { return 1.0 - 3.0 * aA2 + 3.0 * aA1 };
+  function B(aA1, aA2) { return 3.0 * aA2 - 6.0 * aA1 };
+  function C(aA1)      { return 3.0 * aA1 };
+
+  function calcBezier(aT, aA1, aA2) { return ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT };
+  function getSlope(aT, aA1, aA2) { return 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1) };
+
+  function binarySubdivide(aX, aA, aB, mX1, mX2) {
+    let currentX, currentT, i = 0;
+    do {
+      currentT = aA + (aB - aA) / 2.0;
+      currentX = calcBezier(currentT, mX1, mX2) - aX;
+      if (currentX > 0.0) { aB = currentT } else { aA = currentT };
+    } while (Math.abs(currentX) > 0.0000001 && ++i < 10);
+    return currentT;
+  }
+
+  function newtonRaphsonIterate(aX, aGuessT, mX1, mX2) {
+    for (let i = 0; i < 4; ++i) {
+      const currentSlope = getSlope(aGuessT, mX1, mX2);
+      if (currentSlope === 0.0) return aGuessT;
+      const currentX = calcBezier(aGuessT, mX1, mX2) - aX;
+      aGuessT -= currentX / currentSlope;
+    }
+    return aGuessT;
+  }
+
+  function BezierEasing(mX1, mY1, mX2, mY2) {
+
+    if (!(0 <= mX1 && mX1 <= 1 && 0 <= mX2 && mX2 <= 1)) return;
+    let sampleValues = new Float32Array(kSplineTableSize);
+
+    if (mX1 !== mY1 || mX2 !== mY2) {
+      for (let i = 0; i < kSplineTableSize; ++i) {
+        sampleValues[i] = calcBezier(i * kSampleStepSize, mX1, mX2);
+      }
+    }
+
+    function getTForX(aX) {
+
+      let intervalStart = 0;
+      let currentSample = 1;
+      const lastSample = kSplineTableSize - 1;
+
+      for (; currentSample !== lastSample && sampleValues[currentSample] <= aX; ++currentSample) {
+        intervalStart += kSampleStepSize;
+      }
+
+      --currentSample;
+
+      const dist = (aX - sampleValues[currentSample]) / (sampleValues[currentSample + 1] - sampleValues[currentSample]);
+      const guessForT = intervalStart + dist * kSampleStepSize;
+      const initialSlope = getSlope(guessForT, mX1, mX2);
+
+      if (initialSlope >= 0.001) {
+        return newtonRaphsonIterate(aX, guessForT, mX1, mX2);
+      } else if (initialSlope === 0.0) {
+        return guessForT;
+      } else {
+        return binarySubdivide(aX, intervalStart, intervalStart + kSampleStepSize, mX1, mX2);
+      }
+
+    }
+
+    return x => {
+      if (mX1 === mY1 && mX2 === mY2) return x;
+      if (x === 0 || x === 1) return x;
+      return calcBezier(getTForX(x), mY1, mY2);
+    }
+
+  }
+
+  return BezierEasing;
+
+})();
+
+function smoothExit(t) {
+  const easing = BezierEasing(1, 0, .9, 1);
+  return easing(t)
+}
+
+function gentleInflow(t) {
+  const easing = BezierEasing(.25, 0, 0, 1);
+  return easing(t)
+}
+
+function harmoniousEase(t) {
+  const easing = BezierEasing(1, 0, 1, 1);
+  return easing(t)
+}
+
+function getUnit(val) {
+  const split = /[+-]?\d*\.?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?(%|px|pt|em|rem|in|cm|mm|ex|ch|pc|vw|vh|vmin|vmax|deg|rad|turn)?$/.exec(val);
+  if (split) return split[1];
+}
+
+function getCSSValue(el, prop) {
+  if (prop in el.style) {
+    const uppercasePropName = prop.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+    const value = el.style[prop] || getComputedStyle(el).getPropertyValue(uppercasePropName) || '0';
+    return value;
+  }
+}
+
+function dwMotion({duration = 1000, delay = 0, easing, draw, callback = null}) {
+  const startTime = performance.now();
+
+  requestAnimationFrame(function animate(timestamp) {
+    const elapsed = timestamp - startTime;
+
+    if(elapsed < delay) {
+      requestAnimationFrame(animate);
+      return;
+    }
+
+    const progress = Math.min(1, (elapsed - delay) / duration);
+
+    const easedProgress = easing ? easing(progress) : progress;
+
+    draw(easedProgress);
+
+    if(progress < 1) {
+      requestAnimationFrame(animate);
+    } else {
+      if(callback) {
+        callback();
+      }
+    }
+  });
+}
+
 const defaultSettings = {
   target: null,
   width: "100%",
@@ -191,6 +329,7 @@ function dwtlSlideIt() {
   }
   dwtlSetActiveClass(elements.navigations, settings.currentEvent);
   dwtlSetActiveClass(elements.periods, settings.currentEvent);
+  eventAnimation();
 }
 
 function dwtlNaviClick(event) {
@@ -334,6 +473,77 @@ function dwtlAddEvents() {
   elements.timeline.addEventListener("touchend", dwtlHandleTouchEnd, {passive: false});
 }
 
+function eventAnimation() {
+  const dateTitle = elements.periods[settings.currentEvent].querySelector(".dwtl-event-date-title");
+  const dateSpan = elements.periods[settings.currentEvent].querySelector(".dwtl-event-date").children;
+  const details = elements.periods[settings.currentEvent].querySelector(".dwtl-event-details").children;
+  dwMotion({
+    easing: smoothExit,
+    draw(easedProgress) {
+      const initialValue = 2;
+      const finalValue = 1;
+      const currentValue = ((finalValue - initialValue) * easedProgress) + initialValue;
+      dateTitle.style.transform = "translate(-50%, -50%) scale(" + currentValue + ")";
+    }
+  });
+  Array.from(dateTitle.children).forEach((item, index) => {
+    dwMotion({
+      duration: 800,
+      delay: index * 100,
+      easing: gentleInflow,
+      draw(easedProgress) {
+        const initialValue = 100;
+        const finalValue = 0;
+        const currentValue = ((finalValue - initialValue) * easedProgress) + initialValue;
+        item.style.transform = "translateY(" + currentValue + "%)";
+      },
+      callback: () => {
+        dwMotion({
+          duration: 1000,
+          delay: index * 100,
+          easing: harmoniousEase,
+          draw(easedProgress) {
+            const initialValue = 0;
+            const finalValue = -100;
+            const currentValue = ((finalValue - initialValue) * easedProgress) + initialValue;
+            item.style.transform = "translateY(" + currentValue + "%)";
+          },
+          callback: () => {
+            if(index === 0) {
+              Array.from(dateSpan).forEach((item, index) => {
+                dwMotion({
+                  duration: 1000,
+                  delay: index * 100,
+                  easing: gentleInflow,
+                  draw(easedProgress) {
+                    const initialValue = 100;
+                    const finalValue = 0;
+                    const currentValue = ((finalValue - initialValue) * easedProgress) + initialValue;
+                    item.style.transform = "translateY(" + currentValue + "%)";
+                  }
+                });
+                for(let i = 1; i <= details.length; i++)  {
+                  dwMotion({
+                    duration: 1000,
+                    delay: 200 * i,
+                    easing: gentleInflow,
+                    draw(easedProgress) {
+                      const initialValue = parseFloat(details[i].style.getPropertyValue('--start-position'));
+                      const finalValue = 0;
+                      const currentValue = ((finalValue - initialValue) * easedProgress) + initialValue;
+                      details[i].style.transform = "translateY(" + currentValue + "px)";
+                    }
+                  });
+                }
+              });
+            }
+          }
+        });
+      }
+    });
+  });
+}
+
 function dwTimeLine(params) {
   settings = dwtlReplaceObjectProps(defaultSettings, params);
   
@@ -344,6 +554,7 @@ function dwTimeLine(params) {
   
   dwtlCreateTimeline();
   dwtlAddEvents();
+  eventAnimation();
   console.log(elements);
 }
 
